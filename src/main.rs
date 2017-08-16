7dab3e37bb919f3abe93e418a3969593e670802c
Christopher Lane <lanechr@gmail.com>
2017-08-16 16:22:03 -0700

    Initialize cells with random(ish) starting values

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,20 @@ extern crate gfx_texture;
 extern crate image;
 extern crate piston;
 extern crate piston_window;
+extern crate rand;
 extern crate texture;
 
 use gfx_texture::Texture;
 use image::ImageBuffer;
 use piston::input::*;
 use piston_window::*;
+use rand::Rng;
 use std::time;
 use texture::TextureSettings;
 
 const WIDTH: u32 = 60;  // Width of window, in cells.
-const HEIGHT: u32 = 60;  // Height of window, in cells.
-const CELL_SIZE: u32 = 10;  // Effectively a zoom factor.  CELL_SIZE * WIDTH => width of the window in pixels.
+const HEIGHT: u32 = 30;  // Height of window, in cells.
+const CELL_SIZE: u32 = 20;  // Effectively a zoom factor.  CELL_SIZE * WIDTH => width of the window in pixels.
 const TICK_PERIOD: u32 = 1500;  // The number of milliseconds between each recalculation of the cell grid.
 
 fn main() {
@@ -27,10 +29,21 @@ fn main() {
 	let tset = TextureSettings::new()
 		.mag(texture::Filter::Nearest);
 
+	// Initialize an array of pixels which can be arbitrarily white or black.
+	let mut random_pixels = [0u8; (WIDTH * HEIGHT * 4) as usize];
+	let mut rng = rand::thread_rng();
+	for pixel in random_pixels.chunks_mut(4) {
+		if (rng.next_u32() % 4) != 0 {
+			pixel[0] = 255;
+			pixel[1] = 255;
+			pixel[2] = 255;
+		}
+		pixel[3] = 255;
+	}
+
 	// This is our cell buffer.  When using the iterators provided by ImageBuffer, p.0 and p.1
 	// are the x, y coordinates, and p.2 is the four-byte array that represents RGBA.
-	let buf = vec![0u8; (WIDTH * HEIGHT * 4) as usize];
-	let mut cells = ImageBuffer::from_raw(WIDTH, HEIGHT, buf).unwrap();
+	let mut cells = ImageBuffer::from_raw(WIDTH, HEIGHT, random_pixels.to_vec()).unwrap();
 	let mut tex = Texture::from_image(&mut window.factory, &cells, &tset).unwrap();
 
 	// Time keeping to know when to recalculate the cell grid.
@@ -40,9 +53,9 @@ fn main() {
 	// Don't start the game until the user presses a key.  This gives the "player" an opportunity
 	// to observe the starting state.
 	while let Some(e) = window.next() {
-		// Render the starting position.  (For now, just a blank slate)
-		window.draw_2d(&e, |_, g| {
-			clear([1.0, 1.0, 1.0, 1.0], g);
+		window.draw_2d(&e, |c, g| {
+			clear([0.0, 0.0, 0.0, 1.0], g);
+			image(&tex, c.transform.scale(CELL_SIZE as f64, CELL_SIZE as f64), g);
 		});
 
 		if e.press_args().is_some() {
@@ -67,6 +80,5 @@ fn main() {
 			clear([0.0, 0.0, 0.0, 1.0], g);
 			image(&tex, c.transform.scale(CELL_SIZE as f64, CELL_SIZE as f64), g);
 		});
-
 	}
 }

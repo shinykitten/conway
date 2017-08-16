1ac34d6d87482fc7a7b46a32bb2a2ae3477354d9
Christopher Lane <lanechr@gmail.com>
2017-08-16 13:35:27 -0700

    Added a basic tick to control the recalculation timing

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,13 @@ use gfx_texture::Texture;
 use image::ImageBuffer;
 use piston::input::*;
 use piston_window::*;
+use std::time;
 use texture::TextureSettings;
 
 const WIDTH: u32 = 60;  // Width of window, in cells.
 const HEIGHT: u32 = 60;  // Height of window, in cells.
 const CELL_SIZE: u32 = 10;  // Effectively a zoom factor.  CELL_SIZE * WIDTH => width of the window in pixels.
+const TICK_PERIOD: u32 = 1500;  // The number of milliseconds between each recalculation of the cell grid.
 
 fn main() {
 	let mut window : PistonWindow = WindowSettings::new("conway", [WIDTH * CELL_SIZE, HEIGHT * CELL_SIZE])
@@ -25,24 +27,46 @@ fn main() {
 	let tset = TextureSettings::new()
 		.mag(texture::Filter::Nearest);
 
+	// This is our cell buffer.  When using the iterators provided by ImageBuffer, p.0 and p.1
+	// are the x, y coordinates, and p.2 is the four-byte array that represents RGBA.
 	let buf = vec![0u8; (WIDTH * HEIGHT * 4) as usize];
 	let mut cells = ImageBuffer::from_raw(WIDTH, HEIGHT, buf).unwrap();
 	let mut tex = Texture::from_image(&mut window.factory, &cells, &tset).unwrap();
 
+	// Time keeping to know when to recalculate the cell grid.
+	let tick_period = time::Duration::from_millis(TICK_PERIOD as u64);
+	let mut last_tick = time::Instant::now();
+
+	// Don't start the game until the user presses a key.  This gives the "player" an opportunity
+	// to observe the starting state.
 	while let Some(e) = window.next() {
-		if let Some(Button::Keyboard(key)) = e.press_args() {
-			if key == Key::Space {
-				for p in cells.enumerate_pixels_mut() {
-					if p.0 % 2 == p.1 % 2 {
-						p.2.data = [0xFF, 0xFF, 0xFF, 0xFF];
-					}
-				}
-				tex = Texture::from_image(&mut window.factory, &cells, &tset).unwrap();
-			}
+		// Render the starting position.  (For now, just a blank slate)
+		window.draw_2d(&e, |_, g| {
+			clear([1.0, 1.0, 1.0, 1.0], g);
+		});
+
+		if e.press_args().is_some() {
+			break;
 		}
+	}
+
+	// Main game loop: continue recalculating and rendering once per tick forever.
+	while let Some(e) = window.next() {
+		// Recalculate the cell field once per tick.
+		if last_tick.elapsed() > tick_period {
+			for p in cells.enumerate_pixels_mut() {
+				p.2.data = [0, 0, 0, 255];
+			}
+			tex = Texture::from_image(&mut window.factory, &cells, &tset).unwrap();
+			last_tick = time::Instant::now();
+		}
+
+		// This is the draw step.  In our case it's very simple - clear the window then render
+		// the image buffer as a scaled-up texture.
 		window.draw_2d(&e, |c, g| {
 			clear([0.0, 0.0, 0.0, 1.0], g);
 			image(&tex, c.transform.scale(CELL_SIZE as f64, CELL_SIZE as f64), g);
 		});
+
 	}
 }
